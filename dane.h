563f3d24689d28c98d730793f704a579d2d51531
HIeBuS <hlebowiczjakub@gmail.com>
2025-12-26 22:29:00 +0100

    Zdefiniowanie typow wyliczeniowych oraz struktury Ocalaly w pliku dane.h

--- dane.h
+++ dane.h
@@ -1,4 +1,38 @@
 #ifndef DANE_H
 #define DANE_H
 
+typedef enum {
+    BRAK_SPECJALIZACJI=0,
+    MEDYK,
+    INŻYNIER,
+    ZWIADOWCA,
+    TECHNIK,
+    BIOLOG,
+    MECHANIK,
+    STRATEG,
+    WOJOWNIK,
+    SZPIEG,
+    NAUKOWIEC
+} Specjalizacja;
+
+typedef enum {
+    AKTYWNY = 0,
+    CHORY,
+    RANNY,
+    POZA_SCHRONEM,
+    ZAGINIONY,
+    MARTWY
+} Status;
+
+typedef struct Ocalaly {
+    char imie[50];
+    Specjalizacja rola;
+    int ilosc_racji;
+    int stan_zdrowia;
+    int poziom_zagrozenia;
+    Status stan;
+
+    struct Ocalaly *next;
+} Ocalaly;
+
 #endif
